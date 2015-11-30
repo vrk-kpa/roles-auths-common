38f3ac783218db0aa0482494475ca0eb7cde5166
jkorkalainen <juha.korkalainen@iki.fi>
2015-11-30 17:31:37 +0200

    moved src under common

--- src/main/java/fi/vm/kapa/rova/rest/model/RestIdentifier.java
+++
@@ -1,6 +0,0 @@
-package fi.vm.kapa.rova.rest.model;
-
-public interface RestIdentifier {
-    public static final String ISSUES = "issues";
-    public static final String ADDITIONAL_INFO = "additionalInfo";
-}
