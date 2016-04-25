69a6412665d486cd2695a15b69e2d8352a394b42
Mikko Tomperi <mikko.tomperi@cybercom.com>
2016-04-25 16:13:18 +0300

    changed filter to be configurable

--- common/src/main/java/fi/vm/kapa/rova/security/ContentSecurityPolicyFilter.java
+++ common/src/main/java/fi/vm/kapa/rova/security/ContentSecurityPolicyFilter.java
@@ -8,12 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 
 public class ContentSecurityPolicyFilter  extends HeadersAddingFilter {
 
-    private static final Map<String, String> HEADERS = new HashMap<>();
+    private static final Map<String, String> DEFAULT_HEADERS = new HashMap<>();
     static {
-        HEADERS.put("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline';");
+        DEFAULT_HEADERS.put("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline';");
     }
-    
+
+    public ContentSecurityPolicyFilter(Predicate<HttpServletRequest> matcher, Map<String, String> headers) {
+        super(matcher, headers);
+    }
+
     public ContentSecurityPolicyFilter(Predicate<HttpServletRequest> matcher) {
-        super(matcher, HEADERS);
+        super(matcher, DEFAULT_HEADERS);
     }
 }
