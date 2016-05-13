5f287c5d77c3b1a8784f63af4ae980de436663cf
Jari Saukkonen <jari.saukkonen@ambientia.fi>
2016-05-13 10:42:28 +0300

    KAPAROVA-243 Make constant fields final

--- common/src/main/java/fi/vm/kapa/rova/rest/validation/ValidationUtil.java
+++ common/src/main/java/fi/vm/kapa/rova/rest/validation/ValidationUtil.java
@@ -39,7 +39,7 @@ public class ValidationUtil {
     public final static String HASH_HEADER_NAME = "X-RoVa-Hash";
     public final static String TIMESTAMP_HEADER_NAME = "X-RoVa-timestamp";
 
-    public static Logger LOG = Logger.getLogger(ValidationUtil.class);
+    private static final Logger LOG = Logger.getLogger(ValidationUtil.class);
     
     private String apiKey;
     private long requestAliveMillis;
