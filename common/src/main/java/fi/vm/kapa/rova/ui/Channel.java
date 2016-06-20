821d0fa76866fefc4685b5f43fa69d4b076c355f
Tommy Savolainen <tommy.savolainen@codemen.fi>
2016-06-20 10:32:31 +0300

    Muutettu myuotoon missä enumin voi liikuttaa restin yli ja tallentaa kantaan

--- common/src/main/java/fi/vm/kapa/rova/ui/Channel.java
+++ common/src/main/java/fi/vm/kapa/rova/ui/Channel.java
@@ -1,21 +1,50 @@
 
 package fi.vm.kapa.rova.ui;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import fi.vm.kapa.rova.rest.exception.ExceptionType;
+import fi.vm.kapa.rova.rest.exception.SystemException;
+import fi.vm.kapa.rova.rest.exception.SystemException.Key;
+
 public enum Channel {
 
     ADMIN_UI("ADMIN-UI"),
     WEB_API("WEB-API"),
     VARE_UI("VARE-UI");
+    
+    private static class Holder {
+        static Map<String, Channel> MAP = new HashMap<>();
+    }
 
     private String name;
 
     Channel(String name) {
         this.name = name;
+        Holder.MAP.put(name, this);
     }
 
+    @JsonValue
     public String getName() {
+        return name;       
+    }
+    
+    @JsonCreator
+    public static Channel find(String val) {
+        Channel t = Holder.MAP.get(val);
+        if (t == null) {
+            throw new SystemException(ExceptionType.MISSING_PARAMETER)
+            .set(Key.DESCRIPTION, String.format("Unsupported type %s.", val));
+        }
+        return t;
+    }
+    
+    public String toString(){
         return name;
     }
-
 }
 
