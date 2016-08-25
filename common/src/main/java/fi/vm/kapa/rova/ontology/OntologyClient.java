aa06357768a94aec63de9ed32248ab805709786e
jkorkalainen <juha.korkalainen@iki.fi>
2016-08-25 17:09:54 +0300

    OntologyClient initial version

---
+++ common/src/main/java/fi/vm/kapa/rova/ontology/OntologyClient.java
@@ -0,0 +1,44 @@
+package fi.vm.kapa.rova.ontology;
+
+import fi.vm.kapa.rova.rest.AbstractClient;
+
+import javax.ws.rs.core.GenericType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.text.MessageFormat.format;
+
+/**
+ * Created by Juha Korkalainen on 25.8.2016.
+ */
+public class OntologyClient extends AbstractClient {
+
+    public OntologyClient(String endPointUrl) {
+        super(endPointUrl);
+    }
+
+    public Concept getConcept(String uri) {
+        return getGeneric(format("/rest/concept/{0}", uri), new GenericType<Concept>() {});
+    }
+
+    public List<Concept> getConcepts() {
+        return getGeneric(format("/rest/concept/all"), new GenericType<List<Concept>>() {});
+    }
+
+    public Boolean isBroaderConcept(String broaderUri, String narrowerUri) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("broader-uri", broaderUri);
+        params.put("narrower-uri", narrowerUri);
+        return getGeneric(format("/rest/concept/is-broader-concept", params), new GenericType<Boolean>() {});
+    }
+
+    public Set<Concept> getNarrowerConcepts(String uri) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("broader-uri", uri);
+        return getGeneric(format("/rest/concept/narrower", params), new GenericType<Set<Concept>>() {});
+    }
+
+}
