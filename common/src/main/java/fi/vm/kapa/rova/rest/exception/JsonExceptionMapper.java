8b0f69446f7afe455d28fbcdfd8be83854806a7b
tkarttu <tkarttu@gmail.com>
2016-12-07 11:14:33 +0200

    KAPAROVA-1369 lisatty Json-parsintavirheille uusi mapper, joka ei paljasta responsessa virheen yksityiskohtia

---
+++ common/src/main/java/fi/vm/kapa/rova/rest/exception/JsonExceptionMapper.java
@@ -0,0 +1,42 @@
+/**
+ * The MIT License
+ * Copyright (c) 2016 Population Register Centre
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package fi.vm.kapa.rova.rest.exception;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import fi.vm.kapa.rova.logging.Logger;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class JsonExceptionMapper extends AbstractExceptionMapper<JsonMappingException> {
+    private static final Logger LOG = Logger.getLogger(JsonExceptionMapper.class);
+
+    @Override
+    public Response toResponse(JsonMappingException e) {
+        LOG.error("Unhandled Exception: ", e);
+        // replace error msg with less revealing one
+        return getResponse(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), new JsonMappingException("Invalid JSON data"));
+    }
+
+}
