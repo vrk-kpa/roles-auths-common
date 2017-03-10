adc4ef1d7eafc6db41dc6d6fa637b7a3fbffcf89
Mikko Tomperi <mikko.tomperi@cybercom.com>
2017-03-10 14:52:42 +0200

    added converter class

---
+++ common/src/main/java/fi/vm/kapa/rova/utils/CaseInsensitiveConverter.java
@@ -0,0 +1,20 @@
+package fi.vm.kapa.rova.utils;
+
+import java.beans.PropertyEditorSupport;
+
+public class CaseInsensitiveConverter<T extends Enum<T>> extends PropertyEditorSupport {
+
+    private final Class<T> typeParameterClass;
+
+    public CaseInsensitiveConverter(Class<T> typeParameterClass) {
+        super();
+        this.typeParameterClass = typeParameterClass;
+    }
+
+    @Override
+    public void setAsText(final String text) throws IllegalArgumentException {
+        String upper = text.toUpperCase();
+        T value = T.valueOf(typeParameterClass, upper);
+        setValue(value);
+    }
+}
