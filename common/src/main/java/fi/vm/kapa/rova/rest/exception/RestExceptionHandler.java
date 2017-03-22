095a47d3a4d5a45fa5a9a64122888c53425bc64b
Mikko Tomperi <mikko.tomperi@cybercom.com>
2017-03-22 15:44:12 +0200

    no body for 204 messages

--- common/src/main/java/fi/vm/kapa/rova/rest/exception/RestExceptionHandler.java
+++ common/src/main/java/fi/vm/kapa/rova/rest/exception/RestExceptionHandler.java
@@ -71,6 +71,9 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(WebApplicationException.class)
     protected ResponseEntity<Object> webApplicationException(WebApplicationException e, WebRequest request) {
         LOG.error("WebApplicationException: " + e.toString());
+        if (e.getStatus() == 204) {
+            return handleExceptionInternal(e, null , null, HttpStatus.valueOf(204), request);
+        }
         return handleExceptionInternal(e, buildEntity(e), getJsonHeader(), HttpStatus.valueOf(e.getStatus()), request);
     }
 
